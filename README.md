b22156e5c9d4751769562bcbb0f7025382c34341
ffumuya <maira.silva.costa@escola.pr.gov.br>
2025-08-25 10:50:26 -0300

    criei as listas

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# python-listaDeConvidados
+# python-listaD
 aula
--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-convidados_da_festa = ["mitsuri, obanai']
+# Lista de convidados da festa
+convidados_da_festa = ['Mitsuri','kanao','shinobu']
+
+status_presenca = {}
+
+#listas de pessoas que tentaram entrar
+Lista_de_chegadas = ['Mitsuri','kanao']
